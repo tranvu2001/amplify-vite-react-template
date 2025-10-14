46126bcf0403633cf058aee3d0e480a4e2d9ed3a
unknown <tranvu20022001@gmail.com>
2025-10-14 23:33:25 +0700

    new

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Authenticator, ThemeProvider, defaultTheme } from '@aws-amplify/ui-react';
+
 import App from "./App.jsx";
 // import "./index.css";
 import "./GlobalCSS/basecss.css";
 import '@aws-amplify/ui-react/styles.css';
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
+
+
 import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom'
-import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community'; 
 
 
-Amplify.configure(outputs);
-ModuleRegistry.registerModules([AllCommunityModule]);
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
